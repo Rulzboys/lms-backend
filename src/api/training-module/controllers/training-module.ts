4f1ab9537c4366bf8f66e42e138524edaf513e43
Rulzboys <bhalueng28@gmail.com>
2025-05-25 11:54:58 +0700

    Initial commit for LMS backend

---
+++ src/api/training-module/controllers/training-module.ts
@@ -0,0 +1,7 @@
+/**
+ * training-module controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::training-module.training-module');
---
+++ src/api/training-module/routes/training-module.ts
@@ -0,0 +1,7 @@
+/**
+ * training-module router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::training-module.training-module');
